bff7b4fda6990751f855daebcb8916051514dc3b
FroPyx <42712047+FroPyx@users.noreply.github.com>
2020-11-10 19:32:11 +0100

    Clearing

--- stellar_utilities/include/stellar_position_conversion/StellarPositionConversion.h
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-
-
-// Define the radPositionFormatToHourAnglePositionFormat function
-std::vector<double> radPositionFormatToHourAnglePositionFormat(const std::vector<double>& rad_stellar_position);
-
-
-// Define the radPositionFormatToHourAngleTextPositionFormat function
-std::vector<std::string> radPositionFormatToHourAngleTextPositionFormat(const std::vector<double>& rad_stellar_position);
